6a356f034494166ee246c44d86a3bfb07ac41232
plaird730 <plaird730@gmail.com>
2019-01-14 12:19:52 -0500

    Updated schema to include a username and map field to better match the dataset and allow for scaleability

--- OLD/db/schema.sql
+++ OLD/db/schema.sql
@@ -6,7 +6,9 @@ USE arenadata;
 
 CREATE TABLE matches (
 id integer(11) NOT NULL AUTO_INCREMENT,
-timestamp integer(30) NOT NULL,
+username varchar(36) NOT NULL,
+timeofmatch integer(30) NOT NULL,
+map integer(4),
 playersnumber integer(2) NOT NULL,
 teamcomposition varchar(256) NOT NULL,
 enemycomposition varchar(256) NOT NULL,
@@ -26,11 +28,14 @@ PRIMARY KEY (`id`)
 
 -- Insert dummy department
 Insert Into matches (
-					timestamp, 
+					username,
+					timeofmatch,
+                    map,
 					playersnumber, 
 					teamcomposition, 
-                    enemycomposition, duration, 
-                    duration, victory, 
+                    enemycomposition, 
+                    duration, 
+                    victory, 
                     killingblows, 
                     damage, 
                     healing, 
@@ -40,10 +45,10 @@ Insert Into matches (
                     enemymmr,
                     specialization,
                     israted)
-Values (1547221143,572,6,"MAGE-Frost,PRIEST-Discipline,ROGUE-Assassination","PALADIN-Holy,SHAMAN-Elemental, WARLOCK-Destruction",337,false,0,588988,210871,0,-8,2365,2406,"Frost",true), 
-(1547220783,1134,6,"MAGE-Frost,PRIEST-Discipline,ROGUE-Assassination","DEATHKNIGHT-Unholy,MONK-Mistweaver,PALADIN-Protection",253,true,2,637324,43925,0,13,2344,2301,"Frost",true),
-(1547220265,1134,6,"MAGE-Frost,PRIEST-Discipline,ROGUE-Assassination","DEATHKNIGHT-Unholy,PALADIN-Holy,SHAMAN-Elemental",225,true,0,540753,183417,0,10,2333,2217,"Frost",true),
-(1547219690,1825,6,"MAGE-Frost,PRIEST-Discipline,ROGUE-Assassination","DEATHKNIGHT-Unholy,MONK-Mistweaver,PALADIN-Protection",197,true,2,785612,83026,0,15,2294,2340,"Frost",true),
-(1547219347,980,6,"MAGE-Frost,PRIEST-Discipline,ROGUE-Assassination","DEATHKNIGHT-Unholy,PALADIN-Holy,SHAMAN-Elemental",201,true,1,574144,92786,0,12,2270,2243,"Frost",true);
+Values ("Thelvory",1547221143,572,6,"MAGE-Frost,PRIEST-Discipline,ROGUE-Assassination","PALADIN-Holy,SHAMAN-Elemental, WARLOCK-Destruction",337,false,0,588988,210871,0,-8,2365,2406,"Frost",true), 
+("Thelvory",1547220783,1134,6,"MAGE-Frost,PRIEST-Discipline,ROGUE-Assassination","DEATHKNIGHT-Unholy,MONK-Mistweaver,PALADIN-Protection",253,true,2,637324,43925,0,13,2344,2301,"Frost",true),
+("Thelvory",1547220265,1134,6,"MAGE-Frost,PRIEST-Discipline,ROGUE-Assassination","DEATHKNIGHT-Unholy,PALADIN-Holy,SHAMAN-Elemental",225,true,0,540753,183417,0,10,2333,2217,"Frost",true),
+("Thelvory",1547219690,1825,6,"MAGE-Frost,PRIEST-Discipline,ROGUE-Assassination","DEATHKNIGHT-Unholy,MONK-Mistweaver,PALADIN-Protection",197,true,2,785612,83026,0,15,2294,2340,"Frost",true),
+("Thelvory",1547219347,980,6,"MAGE-Frost,PRIEST-Discipline,ROGUE-Assassination","DEATHKNIGHT-Unholy,PALADIN-Holy,SHAMAN-Elemental",201,true,1,574144,92786,0,12,2270,2243,"Frost",true);
 
-SELECT * FROM products;
+SELECT * FROM matches;
